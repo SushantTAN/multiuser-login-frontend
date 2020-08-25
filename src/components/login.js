53f2ec2ea533e0f611b8d30ddafe4730eb64e4af
sushantTAN <punk1susant@gmail.com>
2020-08-25 18:46:27 +0545

    update: frontend validation, background images

--- src/components/login.js
+++ src/components/login.js
@@ -47,7 +47,19 @@ class Login extends Component {
             })
             .catch(err => {
                 console.log("the error is : " + err);
+                alert(err + " Incorrect email or password");
             })
+    }
+
+    renderErrorPassword(){
+        
+        if(this.state.exampleInputPassword1.length < 6 && this.state.exampleInputPassword1.length !== 0){
+            return(
+                <small className="text-danger">
+                    Name should be atlest 6 letters
+                </small>
+            )
+        }
     }
 
     render() { 
@@ -56,21 +68,32 @@ class Login extends Component {
         }
 
         return ( 
-        <div>
-            <form className="border-dark" onSubmit={this.onS} style={{width: "15rem", position: "absolute", top: "10rem", left: "41%"}}>
+        <div >
+            <div style={{backgroundImage: `url(http://4.bp.blogspot.com/-FyJuSNsRrWs/TibLzmYFU3I/AAAAAAAAK40/0BGwL0v2eyk/s1600/cool+computer+backgrounds-2.jpg)`, 
+        
+        height: "100vh",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        }}> </div>
+            <div >
+            <form className="border-dark" onSubmit={this.onS} style={{width: "15rem", position: "absolute", top: "10rem", left: "40%"}}>
                 <div className="form-group">
-                    <label >Email address</label>
+                    <label className="text-white">Email address</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={this.onC}/>
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <small id="emailHelp" className="form-text text-light">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group">
-                    <label >Password</label>
+                    <label className="text-white">Password</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" onChange={this.onC}/>
+                    {this.renderErrorPassword()}
                 </div>
                 <Link to="/register" >Register for free</Link> <br />
                 <button type="submit" className="btn btn-primary">Submit</button>
    
             </form>
+            </div>
         </div> );
     }
 }
--- src/components/register.js
+++ src/components/register.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Register extends Component {
     constructor(props) {
@@ -35,30 +36,68 @@ class Register extends Component {
             })
             .catch(err => {
                 console.log("the error is : " + err);
+                alert(err)
             })
     }
+
+    renderErrorName(){
+        
+        if(this.state.name.length < 6 && this.state.name.length !== 0){
+            return(
+                <small className="text-danger">
+                    Name should be atlest 6 letters
+                </small>
+            )
+        }
+    }
+
+    renderErrorPassword(){
+        
+        if(this.state.password.length < 6 && this.state.password.length !== 0){
+            return(
+                <small className="text-danger">
+                    Name should be atlest 6 letters
+                </small>
+            )
+        }
+    }
+
 
     render() { 
         return ( 
             <div>
+            <div  style={{backgroundImage: `url(http://4.bp.blogspot.com/-FyJuSNsRrWs/TibLzmYFU3I/AAAAAAAAK40/0BGwL0v2eyk/s1600/cool+computer+backgrounds-2.jpg)`, 
+        
+        height: "100vh",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        }}></div>
                <form className="border-dark mx-auto" onSubmit={this.onS} style={{width: "15rem", position: "absolute", top: "10rem", left: "41%"}}>
                     <div className="form-group">
-                        <label >Username</label>
+                        <label className="text-white">Username</label>
                         <input className="form-control" id="name" onChange={this.onC}/>
+                        {this.renderErrorName()}
                     </div>
                     <div className="form-group">
-                        <label >Email address</label>
+                        <label className="text-white">Email address</label>
                         <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={this.onC}/>
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                        
+                        <small id="emailHelp" className="form-text text-light">We'll never share your email with anyone else.</small>
                     </div>
                     <div className="form-group">
-                        <label >Password</label>
+                        <label className="text-white">Password</label>
                         <input type="password" className="form-control" id="password" onChange={this.onC}/>
+                        {this.renderErrorPassword()}
                     </div>
+
+                    <Link to="/">Already have an account? Log in</Link>
                   
                     <button type="submit" className="btn btn-primary">Submit</button>
    
                 </form>     
+            
             </div>
         );
     }
